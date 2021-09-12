25e52a924c09f14b4b8a1c509eb7b76429b73547
Dennis Cheung <dennis199441@gmail.com>
2021-09-12 22:41:07 +0100

    reduce number of processes

--- app/services/video_service.py
+++ app/services/video_service.py
@@ -1,7 +1,7 @@
 from app.utils.timelapse import run_timelapse
 from app.utils.stabilization import run_stabilization
 from app.services.process_service import get_all_process_types, get_processes_by_type, run_process, complete_process
-from multiprocessing import Pool
+# from multiprocessing import Pool
 import time
 
 NUMBER_OF_WORKERS = 3
@@ -24,9 +24,11 @@ def process_video():
         process_types = get_all_process_types()
         for process_type in process_types:
             processes = get_processes_by_type(process_type["id"])
-            vid_obj = map(
+            vid_objs = map(
                 lambda p: {"id": p["id"], "filename": p["hashed_name"] + "-" + p["filename"]}, processes)
             runner = eval(process_type["name"] + "_runner")
-            with Pool(NUMBER_OF_WORKERS) as p:
-                p.map(runner, vid_obj)
+            # with Pool(NUMBER_OF_WORKERS) as p:
+            #     p.map(runner, vid_objs)
+            for vid_obj in vid_objs:
+                runner(vid_obj)
         time.sleep(60)
